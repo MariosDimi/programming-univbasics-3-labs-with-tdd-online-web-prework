b6b9f9b1c58586c3807e0c552761d9d35f667aa5
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-09-12 01:34:15 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,1 @@
-describe "./calculator.rb" do
-
-  it "contains a local variable called first_number that is assigned to a number" do
-    first_number = "Hello world!" ('./calculator.rb', "first_number")
-
-    expect(first_number).to be_an(Integer).or be_a(Float)
-  end
+first_number = "Hello world!"
